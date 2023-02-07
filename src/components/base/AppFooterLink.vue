5cb9bfd240c1cfda265ca2ff4379fb1ecc3a778c
Julien Sulpis <julien.sulpis@gmail.com>
2023-02-07 21:09:05 +0100

    chore: delete everything

--- src/components/base/AppFooterLink.vue
+++
@@ -1,5 +0,0 @@
-<template functional>
-  <a :href="data.attrs.href" class="font-semibold hover:opacity-75">
-    <slot></slot>
-  </a>
-</template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,52 +1,7 @@
 <template>
-  <div class="flex flex-col">
-    <AppHeader />
-    <main class="flex-grow transition-colors duration-150 bg-default">
-      <nuxt class="container py-16 mx-auto" />
+  <div>
+    <main>
+      <nuxt />
     </main>
-    <AppFooter />
   </div>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-import AppHeader from "@/components/base/AppHeader.vue";
-import AppFooter from "@/components/base/AppFooter.vue";
-import MetaInfoBuilder from "@/utils/MetaInfoBuilder";
-
-export default Vue.extend({
-  head() {
-    let routeName = "index";
-    if (!!this.$route.name) {
-      routeName = this.$route.name.split("_")[0];
-    }
-    // @ts-ignore
-    const i18nSeo = this.$nuxtI18nSeo();
-
-    const customHead = new MetaInfoBuilder()
-      // @ts-ignore
-      .title(this.$t(routeName + ".title"))
-      // @ts-ignore
-      .description(this.$t(routeName + ".description"))
-      .url(process.env.appUrl + this.$route.path)
-      .imageUrl(process.env.appUrl + "/og-image.png")
-      .type("website")
-      .twitterCard("summary_large_image")
-      .build();
-
-    return {
-      title: customHead.title,
-      htmlAttrs: {
-        ...customHead.htmlAttrs,
-        ...i18nSeo.htmlAttrs
-      },
-      meta: [...(i18nSeo.meta || []), ...(customHead.meta || [])],
-      link: [...(i18nSeo.link || []), ...(customHead.link || [])]
-    };
-  },
-  components: {
-    AppHeader,
-    AppFooter
-  }
-});
-</script>
